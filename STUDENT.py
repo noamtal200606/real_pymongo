cf7da050e3f2d81989363cbebb0217b291d17e4e
Moshe Gani <52300144+MosheTDD@users.noreply.github.com>
2025-08-15 12:10:08 +0300

    Update STUDENT.py

--- STUDENT.py
+++ STUDENT.py
@@ -1,8 +1,11 @@
 from pydantic import BaseModel
 
-
+# WHY IS THE NAME OF THE FILE IN ALL CAPS
+# Random space under name
+# I'm pretty sure that 'id' is of type ObjectId for mongo entities. Not sure though so check it.
 class student(BaseModel):
     id: constr(regex=r'^\d{9}$')
     age: int
     name: str
+
     grade: str
